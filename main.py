04cee238d0d92ff95e2e3eb12669a7d1ab4eb504
noamelul <noam.elul28@gimail.com>
2023-03-07 19:06:43 +0200

    init

--- main.py
+++ main.py
@@ -27,72 +27,89 @@ def main():
     walls = [wall1, wall2, wall3, wall4]
 
     if Screen_mode == 'level 1':
-        w_1 = wall(141,11,0,69)
+        w_1 = wall(141, 11, 0, 69)
         # wall_level_1 = [w_1]
     pleyer_image = pleyer_1_image_up
-
-
+    go_left =  False
+    go_down = False
+    steps_down = [step1,step2]
+    go_up = False
+    go_rite = False
 
     while not finish:
+
+        if go_down:
+
+            flur_y -= 5
+            wall1.wall_y -= 5
+            wall2.wall_y -= 5
+            wall3.wall_y -= 5
+            wall4.wall_y -= 5
+            pleyer_image = pleyer_1_image_dound
+        if go_up:
+            for i in range(2):
+                pygame.time.wait(10)
+                limit(flur_x, flur_y, walls)
+                pleyer_image = steps_down[i]
+                bild_level_1(flur_x,flur_y,walls,pleyer_image)
+                pygame.display.flip()
+                flur_y += 5
+                wall1.wall_y += 5
+                wall2.wall_y += 5
+                wall3.wall_y += 5
+                wall4.wall_y += 5
+        if go_rite:
+            pleyer_image = pleyer_1_image_left
+            flur_x -= 5
+            wall1.wall_x -= 5
+            wall2.wall_x -= 5
+            wall3.wall_x -= 5
+            wall4.wall_x -= 5
+        if go_left:
+            pleyer_image = pleyer_1_image_rire
+            flur_x += 5
+            wall1.wall_x += 5
+            wall2.wall_x += 5
+            wall3.wall_x += 5
+            wall4.wall_x += 5
 
         for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
 
-
             if event.type == pygame.QUIT:
                 finish = True
 
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_DOWN:
+                    go_down = True
+                if event.key == pygame.K_UP:
+                    go_up = True
+                if event.key == pygame.K_RIGHT:
+                    go_rite = True
+                if event.key == pygame.K_LEFT:
+                    go_left = True
             if event.type == pygame.KEYUP:
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_DOWN:
-                        pleyer_image = pleyer_1_image_dound
-                        flur_y -= 10
-                        wall1.wall_y -= 10
-                        wall2.wall_y -= 10
-                        wall3.wall_y -= 10
-                        wall4.wall_y -= 10
-
-                    if event.key == pygame.K_UP:
-                        pleyer_image = pleyer_1_image_up
-                        flur_y += 10
-                        wall1.wall_y += 10
-                        wall2.wall_y += 10
-                        wall3.wall_y += 10
-                        wall4.wall_y += 10
-
-                    if event.key == pygame.K_RIGHT:
-                        pleyer_image = pleyer_1_image_left
-                        flur_x -= 10
-                        wall1.wall_x -= 10
-                        wall2.wall_x -= 10
-                        wall3.wall_x -= 10
-                        wall4.wall_x -= 10
-
-                    if event.key == pygame.K_LEFT:
-                        pleyer_image = pleyer_1_image_rire
-                        flur_x += 10
-                        wall1.wall_x += 10
-                        wall2.wall_x += 10
-                        wall3.wall_x += 10
-                        wall4.wall_x += 10
+                go_left = False
+                go_down = False
+                go_up = False
+                go_rite = False
 
 
-                    wall_num = 1
-                    p = limit(flur_x, flur_y, walls)
-                    flur_x = p[0]
-                    flur_y = p[1]
-                    walls = p[2]
-
-                    wall_num = 2
-                    # p = limit(flur_x,flur_y,walls_x[2],walls_y[2])
-                    # flur_x = p[0]
-                    # flur_y = p[1]
-                    # walls_x[2] = p[2]
-                    # walls_y[2] = p[3]
-
-                    pygame.display.flip()
 
 
+
+
+
+
+                p = limit(flur_x, flur_y, walls)
+                flur_x = p[0]
+                flur_y = p[1]
+                walls = p[2]
+
+
+
+                pygame.display.flip()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if ((start_buttons_x_pos <= pos[0] <= start_buttons_x_pos + start_buttons_width) and (
@@ -103,7 +120,7 @@ def main():
             restart_opening_windows()
 
         elif Screen_mode == 'level 1':
-            bild_level_1(flur_x, flur_y, walls,pleyer_image)
+            bild_level_1(flur_x, flur_y, walls, pleyer_image)
 
         pygame.display.flip()
     pygame.quit()
